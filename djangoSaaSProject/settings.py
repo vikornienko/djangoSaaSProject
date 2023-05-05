89d8847b1112d3526966577c7b15825875e99dad
vikornienko <vikornienko76@gmail.com>
2023-05-05 20:46:09 +0500

    Second commit Was add core app.

--- djangoSaaSProject/settings.py
+++ djangoSaaSProject/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Project apps
+    'core.apps.CoreConfig',
 ]
 
 MIDDLEWARE = [
