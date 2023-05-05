34352853e7bd07565dce05835c3ef8b5940b27d3
vikornienko <vikornienko76@gmail.com>
2023-05-05 21:20:05 +0500

    Third commit Made a view, a template and a route for the core application. Tested by selenium.

--- test_functional.py
+++ test_functional.py
@@ -1,4 +1,7 @@
+import time
+
 from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.by import By
 
 from django.test import LiveServerTestCase
 
@@ -7,7 +10,7 @@ class Hosttest(LiveServerTestCase):
     def setUpClass(cls):
         super().setUpClass()
         cls.driver = WebDriver("./other_files/cdriver/chromedriver/chromedriver")
-        cls.driver.implicitly_wait(10)
+        cls.driver.implicitly_wait(5)
 
     @classmethod
     def tearDownClass(cls):
@@ -17,6 +20,11 @@ class Hosttest(LiveServerTestCase):
     def test_home_page(self):
         # self.driver.get(f"{self.live_server_url}/")
         self.driver.get('http://127.0.0.1:8000')
+        # time.sleep(2)
         self.driver.save_screenshot("./other_files/screenshots/image.png")
-        page_title = "The install worked successfully! Congratulations!"
-        assert page_title in self.driver.title
+        # page_title = "The install worked successfully! Congratulations!"
+        page_h1 = "Hello world!"
+
+        # assert page_title in self.driver.title
+
+        assert page_h1 in self.driver.find_element(By.TAG_NAME, "h1").text
