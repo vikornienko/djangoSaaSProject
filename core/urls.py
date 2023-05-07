7be5aaf49befa7e686c33da2a2a4c11ed3172683
vikornienko <vikornienko76@gmail.com>
2023-05-07 12:15:55 +0500

    Fourth commit Added templates for the homepage and other linked pages in the menu. Added views.

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import index
+from .views import about, index, pricing
 
 app_name = 'core'
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', index, name="index"),
+    path('about/', about, name="about"),
+    path('pricing/', pricing, name="pricing"),
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "core/index.html")
+
+def about(request):
+    return render(request, "core/about.html")
+
+def pricing(request):
+    return render(request, "core/pricing.html")
