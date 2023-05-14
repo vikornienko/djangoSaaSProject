c91059db439cf72c58109de7b191cb7d8e3f39b2
vikornienko <vikornienko76@gmail.com>
2023-05-14 16:23:55 +0500

    The eleventh commit Limit for the number of links

--- appaccounts/models.py
+++ appaccounts/models.py
@@ -2,5 +2,10 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+class Plan(models.Model):
+    name = models.CharField(max_length=64)
+    max_num_links = models.PositiveIntegerField()
+
 class CustomUser(AbstractUser):
-    pass
+    plan = models.ForeignKey(Plan, related_name="users", on_delete=models.CASCADE)
+
